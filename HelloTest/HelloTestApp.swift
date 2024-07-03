3948c126245456562cd8c2bbc1adb59bef067d68
langpeu <icopy@naver.com>
2024-07-03 17:11:39 +0900

    Initial Commit

---
+++ HelloTest/HelloTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTestApp.swift
+//  HelloTest
+//
+//  Created by Langpeu on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloTestUITests/HelloTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HelloTestUITestsLaunchTests.swift
+//  HelloTestUITests
+//
+//  Created by Langpeu on 7/3/24.
+//
+
+import XCTest
+
+final class HelloTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
